1a6e9dd300f1c5f9c492fccc19b88a32e487f660
Sneha Raut <sneharaut283@gmail.com>
2024-04-23 09:30:02 +0545

    new chnages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const server = express();
 const { Client } = require("pg");
 
 
+
 const staticPath = path.join(__dirname, "../public");
 server.use(express.static(staticPath));
 
@@ -22,26 +23,27 @@ const client = new Client({
 client.connect();
 //clock in logic
 server.post("/clock-in", async (req, res) => {
-    try {
-       
-        const currentTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
-
-
-
-        const query = "INSERT INTO attendance (clock_in) VALUES ($1)";
-        const values = [currentTime];
-
-
-        await client.query(query, values);
-        console.log("Clock-in time inserted successfully");
-
-       
-        res.sendStatus(200);
-    } catch (error) {
-        console.error("Error inserting clock-in time:", error);
-        res.sendStatus(500);
-    }
-});
+        try {
+           
+            const currentTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
+    
+    
+    
+            const query = "INSERT INTO attendance (clock_in) VALUES ($1)";
+            const values = [currentTime];
+    
+    
+            await client.query(query, values);
+            console.log("Clock-in time inserted successfully");
+    
+           
+            res.sendStatus(200);
+        } catch (error) {
+            console.error("Error inserting clock-in time:", error);
+            res.sendStatus(500);
+        }
+    });
+    
 
 //clock out logic
 server.post("/clock-out", async (req, res) => {
@@ -100,3 +102,4 @@ server.get("/attendance", async (req, res) => {
 
 const PORT = 8080;
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
