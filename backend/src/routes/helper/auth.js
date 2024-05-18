eafeac871d8cc6d1d0d36eb3ac669f0458135608
Sanil Shakya <sanilshakya9@gmail.com>
2024-05-18 17:32:47 +0545

    added team request verification feature

--- backend/src/routes/helper/auth.js
+++ backend/src/routes/helper/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
     const token = req.cookies['jwt']
-    if (!token) return res.sendStatus(401)
+    if (!token) return res.redirect("/login")
   
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) return res.redirect("/login")
